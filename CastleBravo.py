32328cbf7bcd67b0cadc4ac211d1d9c5528c7e69
Ryland <02justin.ryland@gulllakecs.org>
2024-10-08 08:26:34 -0400

    Added While Loop for our OS booting up w/ellipses

--- CastleBravo.py
+++ CastleBravo.py
@@ -1,1 +1,19 @@
+import sys
+import time
+
 print("\nWelcome to InfoTechCenter V1.0")
+
+
+x = 0
+ellipses = 0
+
+while x != 20:
+    x += 1
+    message = ("InfoTech Center System Booting" + "." * ellipses)
+    ellipses += 1
+    sys.stdout.write("\r" + message)
+    time.sleep(.5)
+    if ellipses == 4:
+        ellipses = 0
+    if x == 20:
+        print("\n\nOperating System Booted Up - Retina Scanned - Access Granted\n")
