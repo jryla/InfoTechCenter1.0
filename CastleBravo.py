2c12ba54761ad4130983d29bd56a60f474b5d72b
Ryland <02justin.ryland@gulllakecs.org>
2024-10-11 07:37:30 -0400

    Added Colors to Print

--- CastleBravo.py
+++ CastleBravo.py
@@ -1,19 +1,22 @@
 import sys
 import time
 
-print("\nWelcome to InfoTechCenter V1.0")
+green = "\033[92m"
+bold = "\033[1m"
+reset_color = "\033[0m"  # ANSI escape code to reset to default color
 
+print(f"{bold}\n\tWelcome to InfoTechCenter v1.0{reset_color}")  # Purple text for the welcome message
 
 x = 0
 ellipses = 0
 
 while x != 20:
     x += 1
-    message = ("InfoTech Center System Booting" + "." * ellipses)
+    message = (f"{green}InfoTech Center System Booting{reset_color}" + f"{green}.{reset_color}" * ellipses)
     ellipses += 1
     sys.stdout.write("\r" + message)
     time.sleep(.5)
     if ellipses == 4:
         ellipses = 0
     if x == 20:
-        print("\n\nOperating System Booted Up - Retina Scanned - Access Granted\n")
+        print(f"\n\n\n{bold}OS Booted Up - Retina Scanned - Access Granted{reset_color}")
