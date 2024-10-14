449fbc53a4f91af795ff4080d0f167d3a6bfcc32
Ryland <02justin.ryland@gulllakecs.org>
2024-10-14 08:40:33 -0400

    Added the VehicleRS function - which calls the weatheralert variable - Stable

--- CastleBravo.py
+++ CastleBravo.py
@@ -7,12 +7,14 @@ import random
 from time import sleep
 
 def weather():
-    weatherforecast = ["snowing", "blizzard", "raining", "windy", "icy", "sunny"]
+    weatherforecast = ["snowy", "blizzard", "rainy", "windy", "icy", "sunny"]
     weathercondition = random.choice(weatherforecast)
     return weathercondition
 
-print(weather())
+weatheralert = weather()
 
+def vehicleRS():
+    if weatheralert == "snowy":
+        print("\nThe National Weather service has updated our alarm by 30 minutes \nof the forecast of", weatheralert, "weather conditions.")
 
-
-
+vehicleRS()
